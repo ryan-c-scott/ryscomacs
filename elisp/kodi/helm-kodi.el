0dbba62ce192154afc2586e3dc0a704a65ddaea8
Ryan C. Scott <ryan@5pmCasual.com>
2016-01-04 00:59:06 -0800

    Initial checkin of my Kodi interaction modes

---
+++ elisp/kodi/helm-kodi.el
@@ -0,0 +1,89 @@
+(require 'json)
+
+(defvar kodi-host)
+
+(defvar get-shows-query "{\"jsonrpc\": \"2.0\", \"method\": \"VideoLibrary.GetTVShows\", \"params\": { \"properties\": [], \"sort\": { \"order\": \"ascending\", \"method\": \"label\", \"ignorearticle\": true } }, \"id\": \"libTvShows\"}")
+
+(defvar get-episodes-query "{\"jsonrpc\": \"2.0\", \"method\": \"VideoLibrary.GetEpisodes\", \"params\": { \"tvshowid\": %d, \"properties\": [\"season\", \"episode\", \"title\"%s], \"sort\": { \"order\": \"ascending\", \"method\": \"label\", \"ignorearticle\": true } }, \"id\": \"libTvShows\"}")
+
+(defvar get-movies-query "{\"jsonrpc\": \"2.0\", \"method\": \"VideoLibrary.GetMovies\", \"params\": { \"properties\" : [], \"sort\": { \"order\": \"ascending\", \"method\": \"label\", \"ignorearticle\": true } }, \"id\": \"libMovies\"}")
+
+(defun helm-kodi-input-filter (proc content)
+  "Process filter that continually attempts to process the collected response as json.
+On success the function pointed to by 'helm-kodi-results-callback' is called with the json object."
+  (with-current-buffer "*kodi-connection*"
+    ;(insert content)
+    (setq helm-kodi-temp-results (concat helm-kodi-temp-results content))
+    (funcall helm-kodi-results-callback (json-read-from-string helm-kodi-temp-results))
+    (setq helm-kodi-temp-results ""))
+  (delete-process proc))
+
+(defun helm-kodi-handle-shows (results)
+  "Internal input filter callback function that processes show listing results and engages helm with that as a resource."
+  (let ((shows (mapcar (lambda (elt)`( ,(cdr (assoc 'label elt)) . ,(cdr (assoc 'tvshowid elt))))
+                       (cdr (assoc 'tvshows (cdar results))))))
+    (helm :sources '((name . "KODI: Shows")
+                     (candidates . shows)
+                     (action . (("Episodes" . (lambda (candidate) (helm-kodi-episodes candidate)))
+                                ("Plots" . (lambda (candidate) (helm-kodi-episodes candidate t)))))))))
+
+(defun helm-kodi-handle-episodes (results)
+  "Internal input filter callback function that processes episode listing results and engages helm with that as a resource."
+  (let ((episodes (mapcar (lambda (elt)`( ,(concat
+                                            (format "s%de%d. %s:  "
+                                                    (cdr (assoc 'season elt))
+                                                    (cdr (assoc 'episode elt))
+                                                    (cdr (assoc 'title elt)))
+                                            (cdr (assoc 'plot elt))) . ,(cdr (assoc 'episodeid elt))))
+                          (cdr (assoc 'episodes (cdar results))))))
+    (helm :sources '((name . "KODI: Episodes")
+                     (candidates . episodes)
+                     (action . (lambda (candidate) (helm-kodi-play-file (format "\"episodeid\":%d" candidate))))))))
+
+(defun helm-kodi-handle-movies (results)
+  ""
+  (let ((movies (mapcar (lambda (elt)`( ,(cdr (assoc 'label elt)) . ,(cdr (assoc 'movieid elt))))
+                       (cdr (assoc 'movies (cdar results))))))
+    (helm :sources '((name . "KODI: Movies")
+                     (candidates . movies)
+                     (action . (lambda (candidate) (helm-kodi-play-file (format "\"movieid\":%d" candidate))))))))
+
+
+(defun helm-kodi-shows ()
+  (interactive)
+  "Queries Kodi for a list of shows"
+    (let ((stream (open-network-stream "kodi-connection" "*kodi-connection*" kodi-host 9090)))
+      (with-current-buffer (get-buffer "*kodi-connection*") (erase-buffer))
+      (setq helm-kodi-temp-results "")
+      
+      (setq helm-kodi-results-callback 'helm-kodi-handle-shows)
+      (set-process-filter stream 'helm-kodi-input-filter)
+      (process-send-string stream get-shows-query)))
+
+(defun helm-kodi-episodes (show-id &optional plots)
+  "Queries Kodi for a list of show episodes"
+    (let ((stream (open-network-stream "kodi-connection" "*kodi-connection*" kodi-host 9090)))
+      (with-current-buffer (get-buffer "*kodi-connection*") (erase-buffer))
+      (setq helm-kodi-temp-results "")
+      
+      (setq helm-kodi-results-callback 'helm-kodi-handle-episodes)
+      (set-process-filter stream 'helm-kodi-input-filter)
+      (process-send-string stream (format get-episodes-query show-id (if plots ",\"plot\"" "")))))
+
+(defun helm-kodi-movies ()
+  (interactive)
+  "Queries Kodi for a list of movies"
+    (let ((stream (open-network-stream "kodi-connection" "*kodi-connection*" kodi-host 9090)))
+      (with-current-buffer (get-buffer "*kodi-connection*") (erase-buffer))
+      (setq helm-kodi-temp-results "")
+      
+      (setq helm-kodi-results-callback 'helm-kodi-handle-movies)
+      (set-process-filter stream 'helm-kodi-input-filter)
+      (process-send-string stream get-movies-query)))
+
+(defun helm-kodi-play-file (id)
+  "Calls Player.Open RPC.  'id' should be contents of the RPC's 'item' parameter.  e.g. \"movied\":3   or   \"episodeid\":300"
+  (let ((stream (open-network-stream "kodi-connection" "*kodi-connection*" kodi-host 9090)))
+    (set-process-filter stream nil)
+    (process-send-string stream (format "{\"jsonrpc\": \"2.0\", \"method\": \"Player.Open\", \"params\": { \"item\":{%s}}}" id))
+    (delete-process stream)))
---
+++ elisp/kodi/kodi.el
@@ -0,0 +1,156 @@
+;;; kodi-mode.el --- major mode for interacting with a Kodi instance
+
+(require 'multi)
+
+(defvar kodi-mode-connection nil)
+
+(defvar kodi-mode-hook nil
+  "")
+
+(defvar kodi-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "<left>") 'kodi-nav-left)
+    (define-key map (kbd "<right>") 'kodi-nav-right)
+    (define-key map (kbd "<up>") 'kodi-nav-up)
+    (define-key map (kbd "<down>") 'kodi-nav-down)
+    (define-key map (kbd "<backspace>") 'kodi-nav-back)
+    (define-key map (kbd "RET") 'kodi-nav-select)
+    (define-key map (kbd "SPC") 'kodi-play-pause)
+    (define-key map (kbd "C-SPC") 'kodi-stop)
+    (define-key map (kbd "C-s") 'helm-kodi-shows)
+    (define-key map (kbd "C-m") 'helm-kodi-movies)
+    map)
+  "Keymap for Kodi major mode.")
+
+(define-derived-mode kodi-mode nil "Kodi"
+  "Major mode for interacting with Kodi
+\\{kodi-mode-map}
+")
+
+(defvar kodi-mode-connection-input "")
+
+(defun kodi-create-packet (method &optional params)
+  ""
+  (let ((packet `(("jsonrpc" . "2.0")("method" . ,method))))
+    (when params (setq packet (append packet `(("params" . ,params)))))
+    (json-encode packet)))
+
+(defun kodi-input-filter (proc content)
+  (with-current-buffer "*kodi-client*"
+    (setq kodi-mode-connection-input (concat kodi-mode-connection-input content))
+    (let* ((result (json-read-from-string kodi-mode-connection-input))
+	   (method (cdr (assoc 'method result))))
+
+      (message method)
+      (setq kodi-mode-connection-input "")
+      (kodi-response-handler method result))))
+
+(defmulti kodi-response-handler (x &rest _)
+  ""
+  x)
+
+(defmulti-method kodi-response-handler "Player.OnPlay" (_ data)
+  (message "OnPlay received"))
+
+(defmulti-method kodi-response-handler "Player.OnPause" (_ data)
+  (message "OnPause received"))
+
+(defmulti-method-fallback kodi-response-handler (&rest data)
+  (message "Unhandled data: %s" data))
+
+(defun kodi-connect ()
+  (interactive)
+  ""
+  (kodi-disconnect)
+  (let ((stream (open-network-stream "kodi-client" "*kodi-client*" kodi-host 9090)))
+    (setq kodi-mode-connection stream)
+    (with-current-buffer "*kodi-client*" (erase-buffer))
+    (set-process-filter stream 'kodi-input-filter))
+  (switch-to-buffer "*kodi-client*")
+  (kodi-mode))
+
+(defun kodi-disconnect ()
+  (interactive)
+  ""
+  (when kodi-mode-connection (delete-process kodi-mode-connection) (setq kodi-mode-connection nil)))
+
+(defun kodi-play-pause ()
+  (interactive)
+  ""
+  (process-send-string kodi-mode-connection (kodi-create-packet "Player.PlayPause" '(("playerid" . 1)))))
+
+(defun kodi-stop ()
+  (interactive)
+  ""
+  (process-send-string kodi-mode-connection (kodi-create-packet "Player.Stop" '(("playerid" . 1)))))
+
+(defun kodi-nav (cmd)
+  (interactive)
+  ""
+  (process-send-string kodi-mode-connection (kodi-create-packet (format "Input.%s" cmd))))
+
+(defun kodi-nav-left ()
+  (interactive)
+  ""
+  (kodi-nav "Left")
+  (kodi-seek-small-backward))
+
+(defun kodi-nav-right ()
+  (interactive)
+  ""
+  (kodi-nav "Right")
+  (kodi-seek-small-forward))
+
+(defun kodi-nav-up ()
+  (interactive)
+  ""
+  (kodi-nav "Up")
+  (kodi-seek-big-forward))
+
+(defun kodi-nav-down ()
+  (interactive)
+  ""
+  (kodi-nav "Down")
+  (kodi-seek-big-backward))
+
+(defun kodi-nav-back ()
+  (interactive)
+  ""
+  (kodi-nav "Back"))
+
+(defun kodi-nav-select ()
+  (interactive)
+  ""
+  (kodi-nav "Select"))
+
+(defun kodi-seek (cmd)
+  (interactive)
+  ""
+  (process-send-string kodi-mode-connection (kodi-create-packet "Player.Seek" `(("playerid" . 1)("value" . ,cmd)))))
+
+(defun kodi-seek-small-forward ()
+  (interactive)
+  ""
+  (kodi-seek "smallforward"))
+
+(defun kodi-seek-small-backward ()
+  (interactive)
+  ""
+  (kodi-seek "smallbackward"))
+
+(defun kodi-seek-big-forward ()
+  (interactive)
+  ""
+  (kodi-seek "bigforward"))
+
+(defun kodi-seek-big-backward ()
+  (interactive)
+  ""
+  (kodi-seek "bigbackward"))
+
+(defun kodi-disable-subtitles ()
+  (interactive)
+  ""
+  (process-send-string kodi-mode-connection (kodi-create-packet "Player.SetSubtitle" '(("playerid" . 1)("subtitle" . "off")))))
+
+(provide 'kodi)
