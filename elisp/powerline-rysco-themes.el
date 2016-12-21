cfa2d8ddd44b9b6b253d9c397a5934f9e725e61c
Ryan C. Scott <ryan@5pmCasual.com>
2016-12-20 21:29:50 -0800

    Reformatted the powerline-rysco-theme so it's less whitespace crazy

--- elisp/powerline-rysco-themes.el
+++ elisp/powerline-rysco-themes.el
@@ -1,73 +1,73 @@
 (defface powerline-rysco-active3 '((t (:foreground "black")))
-  "Powerline face 3."
-  :group 'powerline)
+	 "Powerline face 3."
+	 :group 'powerline)
 
 (defface powerline-rysco-inactive3 '((t (:foreground "black")))
-  "Powerline face 3."
-  :group 'powerline)
+	 "Powerline face 3."
+	 :group 'powerline)
 
-; NOTE:  eval-defun will handle re-evaluating a defface immediately
+					; NOTE:  eval-defun will handle re-evaluating a defface immediately
 ;(defface powerline-rysco-backing '((t (:foreground "black" :background "firebrick4"))) "" :group 'powerline)
 ;(defface powerline-rysco-backing '((t (:foreground "black" :background "IndianRed4"))) "" :group 'powerline)
-;(defface powerline-rysco-backing '((t (:foreground "black" :background "dark slate grey"))) "" :group 'powerline) ;Matches zsh config
 ;(defface powerline-rysco-backing '((t (:foreground "black" :background "goldenrod"))) "" :group 'powerline)
 (defface powerline-rysco-backing '((t (:foreground "black" :background "dark slate grey")))
-  "Powerline face 3."
-  :group 'powerline)
+	 "Powerline face 3."
+	 :group 'powerline)
 
 (defun powerline-rysco-theme ()
   "Setup the default mode-line."
   (interactive)
-  (setq-default mode-line-format
-                '("%e"
-                  (:eval
-                   (let* ((active (powerline-selected-window-active))
-                          (mode-line (if active 'mode-line 'mode-line-inactive))
-                          (face1 (if active 'powerline-active1 'powerline-inactive1))
-			  ;(face2 (if active 'powerline-active2 'powerline-inactive2))
-			  (face2 'powerline-rysco-backing)
-			  ;(face3 (if active 'powerline-rysco-active3 'powerline-rysco-inactive3))
-			  (face3 nil)
-                          (separator-left (intern (format "powerline-%s-%s"
-							  (powerline-current-separator)
-                                                          (car powerline-default-separator-dir))))
-                          (separator-right (intern (format "powerline-%s-%s"
-                                                           (powerline-current-separator)
-                                                           (cdr powerline-default-separator-dir))))
-                          (lhs (list (powerline-raw "%*" nil 'l)
-                                     (when powerline-display-buffer-size
-                                       (powerline-buffer-size nil 'l))
-                                     (when powerline-display-mule-info
-                                       (powerline-raw mode-line-mule-info nil 'l))
-				     (powerline-buffer-id face3 'l)
-                                     (when (and (boundp 'which-func-mode) which-func-mode)
-                                       (powerline-raw which-func-format nil 'l))
-                                     (powerline-raw " ")
-                                     (funcall separator-left mode-line face1)
-                                     (when (boundp 'erc-modified-channels-object)
-                                       (powerline-raw erc-modified-channels-object face1 'l))
-                                     (powerline-major-mode face1 'l)
-                                     (powerline-process face1)
-                                     (powerline-narrow face1 'l)
-                                     (powerline-raw " " face1)
-                                     (funcall separator-left face1 face2)
-                                     (powerline-vc face2 'r)
-                                     (when (bound-and-true-p nyan-mode)
-                                       (powerline-raw (list (nyan-create)) face2 'l))))
-                          (rhs (list (powerline-raw global-mode-string face2 'r)
-                                     (funcall separator-right face2 face1)
-				     (unless window-system
-				       (powerline-raw (char-to-string #xe0a1) face1 'l))
-				     (powerline-raw "%4l" face1 'l)
-				     (powerline-raw ":" face1 'l)
-				     (powerline-raw "%3c" face1 'r)
-				     (funcall separator-right face1 mode-line)
-				     (powerline-raw " ")
-				     (powerline-raw "%6p" nil 'r)
-                                     (when powerline-display-hud
-                                       (powerline-hud face2 face1)))))
-		     (concat (powerline-render lhs)
-			     (powerline-fill face2 (powerline-width rhs))
-			     (powerline-render rhs)))))))
+  (setq-default
+   mode-line-format
+   '("%e"
+     (:eval
+      (let* ((active (powerline-selected-window-active))
+	     (mode-line (if active 'mode-line 'mode-line-inactive))
+	     (face1 (if active 'powerline-active1 'powerline-inactive1))
+					;(face2 (if active 'powerline-active2 'powerline-inactive2))
+	     (face2 'powerline-rysco-backing)
+					;(face3 (if active 'powerline-rysco-active3 'powerline-rysco-inactive3))
+	     (face3 nil)
+	     (separator-left (intern (format "powerline-%s-%s"
+					     (powerline-current-separator)
+					     (car powerline-default-separator-dir))))
+	     (separator-right (intern (format "powerline-%s-%s"
+					      (powerline-current-separator)
+					      (cdr powerline-default-separator-dir))))
+	     (lhs (list (powerline-raw "%*" nil 'l)
+			(when powerline-display-buffer-size
+			  (powerline-buffer-size nil 'l))
+			(when powerline-display-mule-info
+			  (powerline-raw mode-line-mule-info nil 'l))
+			(powerline-buffer-id face3 'l)
+			(when (and (boundp 'which-func-mode) which-func-mode)
+			  (powerline-raw which-func-format nil 'l))
+			(powerline-raw " ")
+			(funcall separator-left mode-line face1)
+			(when (boundp 'erc-modified-channels-object)
+			  (powerline-raw erc-modified-channels-object face1 'l))
+			(powerline-major-mode face1 'l)
+			(powerline-process face1)
+			(powerline-narrow face1 'l)
+			(powerline-raw " " face1)
+			(funcall separator-left face1 face2)
+			(powerline-vc face2 'r)
+			(when (bound-and-true-p nyan-mode)
+			  (powerline-raw (list (nyan-create)) face2 'l))))
+	     (rhs (list (powerline-raw global-mode-string face2 'r)
+			(funcall separator-right face2 face1)
+			(unless window-system
+			  (powerline-raw (char-to-string #xe0a1) face1 'l))
+			(powerline-raw "%4l" face1 'l)
+			(powerline-raw ":" face1 'l)
+			(powerline-raw "%3c" face1 'r)
+			(funcall separator-right face1 mode-line)
+			(powerline-raw " ")
+			(powerline-raw "%6p" nil 'r)
+			(when powerline-display-hud
+			  (powerline-hud face2 face1)))))
+	(concat (powerline-render lhs)
+		(powerline-fill face2 (powerline-width rhs))
+		(powerline-render rhs)))))))
 
 (provide 'powerline-rysco-themes)
