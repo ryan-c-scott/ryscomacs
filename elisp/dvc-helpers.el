64eae6eb031e5e81a6422904f0378a03307d9bae
Ryan C. Scott <ryan@5pmCasual.com>
2017-12-22 11:53:01 -0800

    Added a bunch of dvc helpers for marking/delete/removing files .Regex versions of mark/unmark .Deleting files .Helpers for marking/deleting backups

--- elisp/dvc-helpers.el
+++ elisp/dvc-helpers.el
@@ -1,3 +1,5 @@
+(defvar dvc-diff-backups-regex "\\.orig$")
+
 (defun dvc-diff-mark-file-region (start end &optional unmark)
   (interactive "rregion: ")
     (goto-char start)
@@ -25,11 +27,11 @@
       (dvc-diff-unmark-file-region (region-beginning) (region-end))
     (dvc-diff-unmark-file)))
 
-(defun dvc-diff-mark-files-subdirectory (&optional unmark)
-  (interactive)
+(defun dvc-diff-mark-files-matching (&optional regex unmark)
   (save-excursion
     (let ((path "") (line "") (end 0))
-      (setq path (substring-no-properties (thing-at-point 'line t) 4 (current-column)))
+      (when (not regex)
+	(setq path (substring-no-properties (thing-at-point 'line t) 4 (current-column))))
 
       (goto-char (point-min))
       (forward-line 1)
@@ -42,7 +44,8 @@
       
       (while (< (point) end)
 	(let ((before (point)))
-	  (when (s-contains? path line)
+	  (when (if regex (string-match regex line)
+		  (s-contains? path line))
 	    (if unmark
 		(dvc-diff-unmark-file)
 	      (dvc-diff-mark-file)))
@@ -51,9 +54,46 @@
 	    (forward-line 1)))
       	(setq line (thing-at-point 'line t))))))
 
+(defun dvc-diff-mark-files-subdirectory ()
+  (interactive)
+  (dvc-diff-mark-files-matching))
+
 (defun dvc-diff-unmark-files-subdirectory ()
   (interactive)
-  (dvc-diff-mark-files-subdirectory t))
+  (dvc-diff-mark-files-matching nil t))
+
+(defun dvc-diff-mark-files-regex (regex)
+  (interactive "sRegex: ")
+  (dvc-diff-mark-files-matching regex))
+
+(defun dvc-diff-unmark-files-regex (regex)
+  (interactive "sRegex: ")
+  (dvc-diff-mark-files-matching regex t))
+  
+(defun dvc-diff-mark-backups ()
+  (interactive)
+  (dvc-diff-mark-files-matching dvc-diff-backups-regex))
+
+(defun dvc-diff-print-list ()
+  (interactive)
+  (message "%s" (dvc-current-file-list)))
+
+(defun dvc-diff-delete-files-marked ()
+  (interactive)
+  (when (yes-or-no-p (format "Really delete these files? %s" (dvc-current-file-list)))
+    (let ((root (dvc-tree-root)) path)
+      (dolist (filename (dvc-current-file-list))
+	(setq path (format "%s%s" root filename))
+	(delete-file path)
+	(message "Deleted: %s" path)))
+    (dvc-status)))
+
+(defun dvc-diff-delete-backups ()
+  (interactive)
+  (dvc-status)
+  (dvc-diff-mark-backups)
+  (when (string-match dvc-diff-backups-regex (car (dvc-current-file-list)))
+    (dvc-diff-delete-files-marked)))
 
 ;;;;;;;; Fixes for missing functionality
 (defun xhg-dvc-file-diff (&optional path baseref localref noswitch)
