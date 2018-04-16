ea9547f2864eb3ca638654eb6f0c85e751c3dd5b
Ryan C. Scott <ryan@5pmCasual.com>
2018-04-15 23:47:27 -0700

    Lots of hype-important modeline theme changes... because that's important. .I wanted 'yellow' for the modified backing, but it was too intense in too many cases.

--- elisp/powerline-rysco-themes.el
+++ elisp/powerline-rysco-themes.el
@@ -1,17 +1,12 @@
-;; NOTE:  eval-defun will handle re-evaluating a defface immediately
-;;(defface powerline-rysco-backing '((t (:foreground "black" :background "firebrick4"))) "" :group 'powerline)
-;;(defface powerline-rysco-backing '((t (:foreground "black" :background "IndianRed4"))) "" :group 'powerline)
-;;(defface powerline-rysco-backing '((t (:foreground "black" :background "goldenrod"))) "" :group 'powerline)
-
 (defface powerline-rysco-active1 '((t (:background "grey22" :foreground "white" :weight normal)))
   "Powerline face 1."
   :group 'powerline)
 
-(defface powerline-rysco-backing '((t (:foreground "black" :background "dark slate grey" :weight normal)))
+(defface powerline-rysco-backing '((t (:foreground "black" :background "dimgray" :weight normal)))
 	 "Powerline face 3."
 	 :group 'powerline)
 
-(defface powerline-rysco-backing-modified '((t (:foreground "black" :background "goldenrod" :weight normal)))
+(defface powerline-rysco-backing-modified '((t (:foreground "black" :background "lightgray" :weight normal)))
 	 "Powerline face 3."
 	 :group 'powerline)
 
@@ -19,9 +14,15 @@
 	 "Powerline face 3."
 	 :group 'powerline)
 
-(defface powerline-rysco-buffer-id '((t (:foreground "white" :background "black" :weight normal)))
+(defface powerline-rysco-buffer-id '((t (:foreground "white" :background "Turquoise4" :weight normal)))
 	 "Powerline face 3."
 	 :group 'powerline)
+
+(defun powerline-rysco-minor-modes ()
+  (s-join "•"
+          (cl-loop for m in (s-split " " (format-mode-line minor-mode-alist))
+                   if (not (member m '("" "Helm" "Projectile[-]" "ElDoc"))) collect
+                   (s-replace "Projectile" "" m))))
 
 (defun powerline-rysco-theme ()
   "Setup the default mode-line."
@@ -37,37 +38,38 @@
 			  (buffer-read-only 'powerline-rysco-backing-ro)
 			  (t 'powerline-rysco-backing)))
 	     (face3 'powerline-rysco-buffer-id)
-	     (separator-left (intern (format "powerline-%s-%s"
-					     (powerline-current-separator)
-					     (car powerline-default-separator-dir))))
-	     (separator-right (intern (format "powerline-%s-%s"
-					      (powerline-current-separator)
-					      (cdr powerline-default-separator-dir))))
+	     (separator-left (intern "powerline-arrow-left"))
+	     (separator-left-secondary (intern "powerline-rounded-left"))
+	     (separator-right (intern "powerline-rounded-right"))
+	     (separator-right-secondary (intern "powerline-bar-right"))
 
 	     (lhs
 	      (list
-	       (powerline-raw "%z" nil 'l)
+	       (powerline-raw "%z" face3 'l)
 	       ;(powerline-raw "%*" nil 'l)
 	       ;; 	 (powerline-buffer-size nil 'l))
 	       ;; 	 (powerline-raw mode-line-mule-info nil 'l))
 	       (powerline-buffer-id face3 'l)
 	       (when (and (boundp 'which-func-mode) which-func-mode)
 		 (powerline-raw which-func-format nil 'l))
-	       (powerline-raw " ")
-	       (funcall separator-left mode-line face1)
+	       (powerline-raw " " face3)
+
+	       (funcall separator-left face3 face1)
 	       (when (boundp 'erc-modified-channels-object)
 		 (powerline-raw erc-modified-channels-object face1 'l))
 	       (powerline-major-mode face1 'l)
 	       (powerline-process face1)
 	       (powerline-narrow face1 'l)
 	       (powerline-raw " " face1)
-	       (funcall separator-left face1 face2)
+
+	       (funcall separator-left-secondary face1 face2)
+	       (powerline-raw (powerline-rysco-minor-modes) face2 'l)
 	       (powerline-vc face2 'r)))
 
 	     (rhs
 	      (list
 	       (powerline-raw global-mode-string face2 'r)
-	       (funcall separator-right face2 face1)
+	       (funcall separator-right-secondary face2 face1)
 	       (unless window-system
 	       	 (powerline-raw (char-to-string #xe0a1) face1 'l))
 	       (powerline-raw "%4l " face1 'l)
