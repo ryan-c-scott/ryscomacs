b9b7443c919ef124f9f64780bf7844cf1cd1adb2
Ryan C. Scott <ryan@5pmCasual.com>
2019-01-31 01:03:16 -0800

    Moved the local configuration loading even higher and added straight.el logic for loading org-mode (as per the instructions on their site). .The bundled org-mode has the side effect of misreporting its version number.

--- elisp/rysco-core.el
+++ elisp/rysco-core.el
@@ -28,6 +28,44 @@
 (require 'cl)
 (require 'rysco-util)
 
+;; Local config
+(load "localconfig" :missing-ok t)
+
+;; HACK:  Org-mode has issues installing via straight.el
+(when rysco-use-straight-packages
+  (require 'subr-x)
+  (straight-use-package 'git)
+
+  (defun org-git-version ()
+    "The Git version of org-mode.
+Inserted by installing org-mode or when a release is made."
+    (require 'git)
+    (let ((git-repo (expand-file-name
+                     "straight/repos/org/" user-emacs-directory)))
+      (string-trim
+       (git-run "describe"
+                "--match=release\*"
+                "--abbrev=6"
+                "HEAD"))))
+
+  (defun org-release ()
+    "The release version of org-mode.
+Inserted by installing org-mode or when a release is made."
+    (require 'git)
+    (let ((git-repo (expand-file-name
+                     "straight/repos/org/" user-emacs-directory)))
+      (string-trim
+       (string-remove-prefix
+        "release_"
+        (git-run "describe"
+                 "--match=release\*"
+                 "--abbrev=0"
+                 "HEAD")))))
+
+  (provide 'org-version)
+
+  (straight-use-package 'org))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Load paths
 (rysco-add-to-loadpath
@@ -36,9 +74,6 @@
  "kodi/"
  "dired-hacks/"
  "monky")
-
-;; Local config
-(load "localconfig" :missing-ok t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Packages
