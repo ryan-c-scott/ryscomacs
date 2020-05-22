6ad717893a63bcd4181e22d84f172e70dfaf234f
Ryan C. Scott <ryan@5pmcasual.com>
2020-05-21 17:32:52 -0700

    Moved the transients into rysco-transients

---
+++ rysco-transients.el
@@ -0,0 +1,72 @@
+(require 'transient)
+
+;;;;;;;;;;;;;;;;;;;;
+;; Main ryscomacs transient
+(defface rysco-main-transient-title
+  '((t
+     :foreground "white"
+     :weight normal
+     :slant italic
+     :height 1.2
+     :underline t))
+  "Face for Ryscomacs title in main transient"
+  :group 'ryscomacs-faces)
+
+(define-transient-command rysco-main-transient ()
+  "Miscellany"
+  [:description
+   (lambda () (concat
+               (all-the-icons-faicon "registered" :face `(:inherit rysco-main-transient-title :height 0.8 :underline nil))
+               (propertize " Miscellany" 'face 'rysco-main-transient-title)
+               "\n"))
+   ["Desktops"
+    ("db" "Create" rysco-desktop+-create)
+    ("dm" "Load" desktop+-load)]
+
+   ["Windows"
+    ("wc" "Clone & Narrow" rysco-clone-and-narrow)]
+
+   ["Buffer Killing"
+    ("kc" "Clones" rysco-kill-all-clones)
+    ("ka" "All" killall)
+    ("kp" "Projectile" projectile-kill-buffers)
+    ("ki" "IRC" kill-rcirc-buffers)]
+
+   ["Time Management"
+    ("tb" "Bluedot" bluedot)]
+
+   ["Describe"
+    ("dm" "Mode" describe-mode)
+    ("dk" "Key Briefly" describe-key-briefly)
+    ("dc" "Character" describe-char)]
+
+   ["Utility"
+    ("ui" "IRC" rcirc)
+    ("ue" "EShell" eshell)
+    ("un" "New EShell" rysco-eshell-new)]
+
+   ["Help"
+    ("hl" "Lossage" view-lossage)
+    ("hi" "Info" helm-info-info)
+    ("he" "Elisp" helm-info-elisp)
+    ("hc" "CL" helm-info-cl)]])
+
+;; Magit intergation
+(define-transient-command rysco-magit-transient ()
+  "GitHub/Bitbucket helper transient for Magit"
+  ["Goto"
+   ("o" "Origin" rysco-magit-goto-origin)
+   ("b" "Branch" rysco-magit-goto-branch)
+   ("c" "Compare" rysco-magit-goto-compare)]
+  ["Pull Request"
+   ("p" "Create" rysco-magit-pull-request)])
+
+(add-hook 'magit-mode-hook
+          (lambda ()
+            (define-key magit-mode-map ">" 'rysco-magit-transient)
+            (transient-append-suffix 'magit-dispatch
+              "%"
+              '(">" "Goto GH/BB" rysco-magit-transient))))
+
+
+(provide 'rysco-transients)
