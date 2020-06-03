ff3f08e8479623deff659aed595e8c1a322f763e
Ryan C. Scott <ryan@5pmCasual.com>
2020-06-02 17:59:03 -0700

    Added advice to calfw data function to remove duplicate events from multiple calendars .Currently done by hashing the begin/end dates and titles

--- rysco-calendar.el
+++ rysco-calendar.el
@@ -22,6 +22,39 @@
      (save-buffer)
      (kill-buffer))))
 
+(defun rysco-calendar-hash-cfw:event (evt)
+  (secure-hash
+   'md5
+   (format
+    "%s|%s|%s"
+    (cfw:event-start-date evt)
+    (cfw:event-end-date evt)
+    (cfw:event-title evt))))
+
+(defun rysco-calendar-calfw-unique-events (results)
+  (cl-loop
+   with hashes = (make-hash-table :test 'equal)
+
+   for (key . data) in results collect
+   (cond
+    ((equal key 'periods)
+     (cons
+      'periods
+      (cl-loop
+       for (beg end evt) in data
+       as evthash = (rysco-calendar-hash-cfw:event evt)
+       unless (gethash evthash hashes) collect
+       (progn
+         (puthash evthash t hashes)
+         `(,beg ,end ,evt)))))
+
+    (t
+     (let ((evthash (rysco-calendar-hash-cfw:event (car data))))
+       (unless (gethash evthash hashes)
+         (puthash evthash t hashes)
+         (cons key data)))))))
+
+(advice-add 'cfw:contents-merge :filter-return 'rysco-calendar-calfw-unique-events)
 (add-to-list 'god-exempt-major-modes 'cfw:calendar-mode)
 
 ;;;;
