46d528396d0bfcdbba08aeb38035b6836b6ccc34
Ryan C. Scott <ryan@5pmcasual.com>
2021-03-03 23:31:15 -0800

    Removed monky-mode support from rysco-desktop+

--- rysco-desktop+.el
+++ rysco-desktop+.el
@@ -22,12 +22,6 @@
 (defun rysco-desktop+-magit-restore (name &rest args)
   (magit-status-internal (car args)))
 
-(defun rysco-desktop+-monky-save ()
-  `(,(vc-find-root "." ".hg")))
-
-(defun rysco-desktop+-monky-restore (name &rest args)
-  (monky-status (car args)))
-
 (defun rysco-desktop+-disable-auto-save (&rest args)
   (desktop-save-mode-off))
 
@@ -36,7 +30,6 @@
 (advice-add 'desktop+-load :after #'rysco-desktop+-disable-auto-save)
 
 (add-to-list 'desktop+-special-buffer-handlers 'magit-status-mode)
-(add-to-list 'desktop+-special-buffer-handlers 'monky-mode)
 (add-to-list 'desktop+-special-buffer-handlers 'eshell-mode)
 
 (desktop+-add-handler
@@ -44,12 +37,6 @@
   (lambda () (eq major-mode 'magit-status-mode))
   'rysco-desktop+-magit-save
   'rysco-desktop+-magit-restore)
-
-(desktop+-add-handler
-    'monky-mode
-  (lambda () (eq major-mode 'monky-mode))
-  'rysco-desktop+-monky-save
-  'rysco-desktop+-monky-restore)
 
 (desktop+-add-handler
     'eshell-mode
