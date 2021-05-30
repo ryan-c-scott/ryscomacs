6a0cf99f20468b9fcf845f8b5ff5447c801e883e
Ryan C. Scott <ryan@5pmcasual.com>
2021-05-30 02:13:05 -0700

    Added ability to accept both changes in a 3-way merge in ediff

--- rysco-core.el
+++ rysco-core.el
@@ -760,6 +760,21 @@
    'diff-changed nil :background "grey5" :foreground "purple"))
 (eval-after-load "diff-mode" '(custom-diff-colors))
 
+(with-eval-after-load 'ediff
+  (defun ediff-copy-both-to-C ()
+    (interactive)
+
+    (ediff-copy-diff
+     ediff-current-difference nil 'C nil
+     (concat
+      (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+      (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+
+  (add-hook 'ediff-keymap-setup-hook
+            (lambda ()
+              (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+            'add-d-to-ediff-mode-map))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Theme/Modeline
 (if (and rysco-fancy-modeline (image-type-available-p 'svg))
