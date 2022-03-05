302f58a3147b11a168c5a89b215974e47f495ea5
Ryan C. Scott <ryan@5pmcasual.com>
2022-03-05 15:55:04 -0800

    Org store node creation now shows potential existing matches
    .A title confirmation step is used with the helm pattern text as
    initial content
    .Unified how links are inserted so that all forms can handle replacing
    any active region

--- rysco-org-store.el
+++ rysco-org-store.el
@@ -20,17 +20,30 @@
      (add-to-list 'rysco-org-store-templates--processed capture-template))))
 
 (defun rysco-org-store-location ()
-  ;; TODO: Verify that file doesn't exist
-  ;; .Somehow abort capture but visit file?
-  ;; .Need to properly support aborting process
-  (find-file
-   (expand-file-name
-    (format "%s/%s-%s.org"
-            rysco-org-store-directory
-            (format-time-string "%Y%m%d")
-            (s-replace "/" "-"
-                       (setq rysco-org-store-last-entry-name
-                             (read-string "Title: ")))))))
+  (let* (existing-node
+         (title (funcall-interactively
+                 'helm-rysco-org-store-ql
+                 :name "Knowledge Store Query"
+                 :actions `(("Use Existing" . ,(lambda (marker)
+                                                 (setq existing-node t)
+                                                 (buffer-file-name (marker-buffer marker)))))
+                 :sources `(,(helm-build-dummy-source "Custom")))))
+
+    (if (eq helm-exit-status 1)
+        (keyboard-quit)
+
+      ;; TODO: Existing node behavior
+
+      (find-file
+       (if existing-node
+           title
+         (expand-file-name
+          (format "%s/%s-%s.org"
+                  rysco-org-store-directory
+                  (format-time-string "%Y%m%d")
+                  (s-replace "/" "-"
+                             (setq rysco-org-store-last-entry-name
+                                   (read-string "Confirm: " title))))))))))
 
 (defmacro with-store-directory (&rest forms)
   "Lexically binds `org-directory' to `rysco-org-store-directory' and executes FORMS"
@@ -63,17 +76,30 @@
 
 (defun helm-rysco-org-store-query ()
   (interactive)
+  (funcall-interactively
+   'helm-rysco-org-store-ql
+   :name "Knowledge Store Query"
+   :actions `(,@helm-org-ql-actions
+              ("Insert as link" . helm-rysco-org-store--insert-candidates))))
+
+(cl-defun helm-rysco-org-store-ql (&key buffers-files (boolean 'and) (name "helm-org-ql") sources actions)
+  "See: `helm-org-ql'."
+  (interactive (list (current-buffer)))
   (with-store-directory
-   (let ((helm-org-ql-actions
-          (append
-           helm-org-ql-actions
-           '(("Insert as link" . helm-rysco-org-store--insert-candidates)))))
-     (funcall-interactively 'helm-org-ql (org-ql-search-directories-files)
-                            :name "Knowledge Store Query"))))
+   (let ((boolean (if current-prefix-arg 'or boolean))
+         (helm-input-idle-delay helm-org-ql-input-idle-delay)
+         (helm-org-ql-actions actions)
+         (buffers-files (or buffers-files (org-ql-search-directories-files))))
+
+     (helm :prompt (format "Query (boolean %s): " (-> boolean symbol-name upcase))
+           :sources `(,@sources
+                      ,(helm-org-ql-source buffers-files :name name))))))
 
 (defun helm-rysco-org-store--insert-candidates (&optional _)
-  (let* ((all-candidates (helm-marked-candidates :all-sources t))
-         (count (length all-candidates))
+  (rysco-org-store--insert-links (helm-marked-candidates :all-sources t)))
+
+(defun rysco-org-store--insert-links (markers)
+  (let* ((count (length markers))
          (replacing (org-region-active-p))
          (sep (cond
                ((and replacing (= count 1))
@@ -84,8 +110,8 @@
       (delete-region (region-beginning) (region-end)))
 
     (loop
-     for candidate in (helm-marked-candidates :all-sources t) do
-     (insert (rysco-org-store-get-marker-link candidate) sep))))
+     for this-link in markers do
+     (insert (rysco-org-store-get-marker-link this-link) sep))))
 
 (defun rysco-org-store-get-marker-link (marker &optional link-text)
   (rysco-org-store--with-buffer-at-marker marker
@@ -100,8 +126,7 @@
 (defun rysco-org-store-post-capture ()
   (when (and rysco-org-store-insert-after-capture (not org-note-abort))
     (setq rysco-org-store-insert-after-capture nil)
-    ;; TODO: If region is set, replace it with this link and do not insert a newline
-    (insert (rysco-org-store-get-marker-link org-capture-last-stored-marker) "\n")))
+    (rysco-org-store--insert-links `(,org-capture-last-stored-marker))))
 
 (add-hook 'org-capture-mode-hook #'rysco-org-store-capture-create-id)
 (add-hook 'org-capture-after-finalize-hook 'rysco-org-store-post-capture)
