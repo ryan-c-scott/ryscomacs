fc0420794e746682896b9cf58244c433df5a2f49
Ryan C. Scott <ryan@5pmcasual.com>
2022-03-08 23:17:10 -0800

    store: Initial Kindle data import support
    .The kindle data is a mess, so clippings will appear as their own
    entries when a match can't be made
    .Titles are inconsistent between the vocab.db, AuthorProfile.asc
    files, and 'My Clippings.txt' and the vocab.db is favored when it
    exists

--- rysco-store.el
+++ rysco-store.el
@@ -140,4 +140,198 @@
 (add-hook 'org-capture-mode-hook #'rysco-store-capture-create-id)
 (add-hook 'org-capture-after-finalize-hook 'rysco-store-post-capture)
 
+;;;;;;;;;;;;;;;;;;;
+(defun rysco-store-kindle-get-books-vocab (location)
+  (with-temp-buffer
+    (loop
+     with default-directory = location
+     with raw = (shell-command "sqlite3 -readonly -separator \"    \" vocab.db \"select * from book_info;\"" t nil)
+     with data = (s-split "\n" (buffer-string))
+     for line in data
+     as entry = (s-split "    " line)
+     as book = (-let [(_ id _ _ title author) entry]
+                 (when (and id title author)
+                   `(,id ,title ,author)))
+     when book collect book)))
+
+(defun rysco-store-kindle-get-books-author-profiles (location)
+  (loop
+   for f in (directory-files-recursively location "AuthorProfile.profile.*.asc$")
+   as data = (progn
+               (with-temp-buffer
+                 (insert-file-contents (expand-file-name f location))
+                 (let* ((data (json-parse-buffer))
+                        (authors (append (gethash "u" data) nil))
+                        (book-list (append (gethash "o" data) nil))
+                        (book-id (gethash "a" data))
+                        (book (--first (string= (gethash "a" it) book-id) book-list))
+                        (title (when book (gethash "t" book))))
+                   (when title
+                     `(,book-id
+                       ,title
+                       ,(gethash "n" (car authors)))))))
+   when data collect data))
+
+(defun rysco-store-kindle-extract-clippings (file)
+  (with-temp-buffer
+    (insert-file-contents (expand-file-name file))
+    (loop
+     with out = (make-hash-table :test 'equal)
+
+     for raw in (s-split "==========" (buffer-string)) do
+     (pcase raw
+       ((rx
+         ;; HACK: Had to find a way to ignore a zero width unicode character character that on some entries
+         (let title (seq (category ascii) (*? anything))) " (" (let author (*? anything)) ")" "\n"
+         bol "- Your " (let type (*? anything)) " on "
+         (** 0 1 "page " (let page (+ digit)) " | ")
+         "Location " (or (seq (let beg (+ digit)) "-" (let end (+ digit)))
+                         (let beg (+ digit)))
+         " | Added on " (let added (*? anything)) eol
+         (* "\n")
+         (let text (*? anything))
+         (* "\n") eos)
+
+        (let* ((highlights (gethash title out)))
+          (puthash title
+                   (append highlights `((,type ,page (,beg ,(or end beg)) ,added ,text)))
+                   out))))
+     finally return out)))
+
+(defun rysco-store-get-books ()
+  (with-store-directory
+   (loop
+    for book in (org-ql-select
+                  (org-ql-search-directories-files)
+                  '(tags-local "book")
+                  :action 'element-with-markers)
+
+    as title = (car (org-element-property :title book))
+    as marker = (org-element-property :org-marker book)
+    collect
+    `(,(substring-no-properties title)
+      ,marker))))
+
+(defun rysco-store-kindle-get-books (location)
+  (let* ((author-profiles (rysco-store-kindle-get-books-author-profiles (concat location "/documents")))
+         (vocab (rysco-store-kindle-get-books-vocab (concat location "/system/vocabulary")))
+         (clippings (rysco-store-kindle-extract-clippings (concat location "/documents/My Clippings.txt")))
+         matched
+         (books (append
+                 vocab
+                 (loop
+                  for entry in author-profiles
+                  for (id title author) in author-profiles
+                  as existing = (assoc id vocab 'string=)
+
+                  do (push title matched)
+                  unless existing collect entry)
+
+                 ;; Loop through the clippings and create entries for anything not matched above
+                 ;; .Add "Clippings - " to the title
+                 (loop
+                  for title being the hash-keys of clippings
+                  as highlights = (gethash title clippings)
+
+                  when (and (not (member title matched)) highlights) collect
+                  `("???" ,(concat "Clippings - " title) "???" ,highlights)
+                 ))))
+
+    ;; Inject highlights/notes
+    (loop
+     for (id title author highlights) in books
+     as highlights = (or highlights (gethash title clippings))
+     collect `(,id ,title ,author ,highlights))))
+
+(defun rysco-store-insert-books-kindle (books)
+  (loop
+   with existing = (rysco-store-get-books)
+   for (id title author highlights) in books
+   as node = (assoc title existing)
+   as marker = (cadr node)
+
+   ;; Only create initial node if it doesn't exist
+   unless node do
+   (setq marker
+         (with-current-buffer (find-file (rysco-store-create-file-name title))
+           (insert
+            (org-element-interpret-data
+             `((headline
+                (:level 1 :title ,title :tags ("book"))
+                (property-drawer
+                 nil
+                 (node-property (:key AUTHOR :value ,author))
+                 (node-property (:key PUBLISHED))
+                 (node-property (:key AMAZON-ID :value ,id))))
+
+               ,(format "\nhttps://www.amazon.com/dp/%s\n\n" id))))
+
+           (org-id-get-create)
+           (point-marker)))
+
+   ;; If it does exist, create a backup
+   when node do
+   (progn
+     ;; TODO:
+     (message "IMPORTING EXISTING BOOK: %s" title))
+
+   ;; Always create a new "Kindle Notes" headline
+   do
+   (rysco-store--with-buffer-at-marker marker
+     ;; TODO: Should try and go to the end of this node and not the file
+     ;; .Look into options for removing old kindle syncs
+     (goto-char (point-max))
+
+     (when highlights
+       (insert
+        (org-element-interpret-data
+         `((headline
+            (:level 2 :title "Kindle Notes")
+            (property-drawer
+             nil
+             (node-property (:key SYNCED :value ,(format-time-string "%Y-%m-%d:%H:%M")))))
+
+           "\n"
+
+           ,@(loop
+              for (type page location added text) in highlights
+              as timestamp = (format-time-string
+                              "%Y-%m-%d:%H:%M"
+                              (date-to-time added))
+
+              collect
+              `((headline
+                 (:level 3 :title ,(format "pg. %s - loc. %s" (or page "?") location))
+                 (property-drawer
+                  nil
+                  (node-property (:key DATE :value ,timestamp))
+                  (node-property (:key PAGE :value ,page))
+                  (node-property (:key LOCATION :value ,location))))
+                "\n"
+
+                ,(pcase type
+                   ("Highlight"
+                    `(quote-block
+                      nil
+                      ,text
+                      "\n"))
+
+                   ("Note"
+                    text))
+
+                "\n"))))))
+
+     (set-buffer-file-coding-system 'utf-8))))
+
+(defun helm-rysco-store-kindle-import (location)
+  (interactive "D")
+  (helm :prompt "Kindle Import "
+        :sources
+        (helm-build-sync-source "Kindle Books"
+          :candidates (lambda ()
+                        (--map `(,(format "%s:\t%s" (caddr it) (cadr it)) . ,it)
+                               (rysco-store-kindle-get-books location)))
+          :action `(("Insert" . (lambda (&rest _)
+                                  (rysco-store-insert-books-kindle (helm-marked-candidates))))))))
+
 (provide 'rysco-org-store)
