f12937fe51887051687c7139fa800ebd65a401c8
Ryan C. Scott <ryan@5pmcasual.com>
2023-03-18 01:40:14 -0700

    Initial Dockerfile support
    Functional. Completes entire bootstrap and build

---
+++ example/docker-init.el
@@ -0,0 +1,23 @@
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")
+                         ("org" . "http://orgmode.org/elpa/")))
+
+(setq straight-use-symlinks t)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;;
+(add-to-list 'load-path (concat user-emacs-directory "elisp"))
+(require 'rysco-docker-autoloads)
+(require 'rysco-core)
