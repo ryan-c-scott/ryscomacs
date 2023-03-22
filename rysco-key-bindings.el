06416ba4790e1c34c0057857d13f97d36f3f2252
rscott <rscott@roblox.com>
2023-03-22 01:24:41 -0700

    Resolved compile issue with mc/ helpers

--- rysco-key-bindings.el
+++ rysco-key-bindings.el
@@ -80,8 +80,9 @@
 ;; Multiple-Cursors
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'rysco-mc-transient)
-(define-key mc/keymap (kbd "C-c n") 'mc/insert-numbers)
-(define-key mc/keymap (kbd "C-c a") 'mc/insert-letters)
+(with-eval-after-load 'multiple-cursors-core
+  (define-key mc/keymap (kbd "C-c n") 'mc/insert-numbers)
+  (define-key mc/keymap (kbd "C-c a") 'mc/insert-letters))
 
 ;; Helpers for things that have a lot of muscle memory
 (global-set-key (kbd "<escape>d") 'kill-word)
